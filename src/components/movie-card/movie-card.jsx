bea1c79dbc23e31ca8615e7f8f9b197ba4e39fa6
saleira <goncalo.caro@gmail.com>
2024-12-11 17:26:29 +0000

    Added bootstrap-react using Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
 
 export const MovieCard = ({ movieData, onMovieClick}) => {
     return (
-        <div onClick={() => {
-                onMovieClick(movieData);
-            }}
-            >
-                {movieData.Title}
-            </div>
+        <Card className="h-100" style={{ width: '18rem', cursor: "pointer"}} onClick={() => onMovieClick(movieData)} variant="link">
+            <Card.Img variant="top" src={movieData.ImagePath} />
+            <Card.Body>
+                <Card.Title>{movieData.Title}</Card.Title>
+                <Card.Text>{movieData.Director.Name}</Card.Text>
+            </Card.Body>
+        </Card>
     );
 };
 
