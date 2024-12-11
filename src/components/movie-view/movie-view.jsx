7766eefd73b169e99e7d3a80fbe9f0d498ac0013
saleira <goncalo.caro@gmail.com>
2024-12-11 17:27:03 +0000

    Added img to movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,10 @@
+
 export const MovieView = ({movieData, onBackClick}) => {
     return (
         <div>
+            <div>
+                <img className="w-50" src={movieData.ImagePath} />
+            </div>
             <div>
                 <span>Title: </span>
                 <span>{movieData.Title}</span>
@@ -17,7 +21,7 @@ export const MovieView = ({movieData, onBackClick}) => {
                 <span>Director: </span>
                 <span>{movieData.Director.Name}</span>
             </div>
-            <button onClick={onBackClick}>Back</button>
+            <button onClick={onBackClick} className="btn btn-primary" style={{ cursor: "pointer" }}>Back</button>
         </div>
     );
 };
