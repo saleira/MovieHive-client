543eb8462d154050e247e8ae338fc883044be2f2
saleira <goncalo.caro@gmail.com>
2024-12-14 01:05:00 +0000

    Updated files to use react-router

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,37 @@
 import {Col, Row, Button} from "react-bootstrap";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-export const MovieView = ({movieData, onBackClick}) => {
+
+export const MovieView = ({movieData}) => {
+    const {movieId} = useParams();
+
+    const movie = movieData.find((b) => b._id === movieId)
     return (
         <Row className="align-items-center">
             <Col md={6}>
-                <img className="w-100" src={movieData.ImagePath} />
+                <img className="w-100" src={movie.ImagePath} />
             </Col>
             <Col md={6}>
                 <div>
                     <span>Title: </span>
-                    <span>{movieData.Title}</span>
+                    <span>{movie.Title}</span>
                 </div>
                 <div>
                     <span>Description: </span>
-                    <span>{movieData.Description}</span>
+                    <span>{movie.Description}</span>
                 </div>
                 <div>
                     <span>Genre: </span>
-                    <span>{movieData.Genre.Name}</span>
+                    <span>{movie.Genre.Name}</span>
                 </div>
                 <div>
                     <span>Director: </span>
-                    <span>{movieData.Director.Name}</span>
+                    <span>{movie.Director.Name}</span>
                 </div>
-                <Button variant="primary" type="submit" onClick={onBackClick} className="btn btn-primary" style={{ cursor: "pointer" }}>Back</Button>
+                <Link to="/">
+                    <Button variant="primary" type="submit" className="btn btn-primary" style={{ cursor: "pointer" }}>Back</Button>
+                </Link>
             </Col>
         </Row>
     );
